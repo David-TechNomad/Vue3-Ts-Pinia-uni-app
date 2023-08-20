fd24d3e5d3f7289e6ae5e9b798f6f7316d54ab63
dushihong <dushihonga@163.com>
2023-08-20 11:29:54 +0800

    add pagesOrder

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,7 +1,6 @@
 import { useMemberStore } from '@/stores'
 // need to change baseURL
-// const baseURL = "http://0.0.0.0:8040"
-const baseURL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net'
+const baseURL = "http://0.0.0.0:8040"
 // 添加请求拦截器
 const httpInterceptor = {
   // 拦截前触发
