ea23dfb1b42c93e3ec3eebc74b944cd0ac4062b4
dushihong <dushihonga@163.com>
2023-08-22 17:22:33 +0800

    h5 build

---
+++ docs/assets/address.aba9718b.js
@@ -0,0 +1,1 @@
+import{h as e}from"./http.86124fb0.js";const d=d=>e({method:"POST",url:"/member/address",data:d}),r=()=>e({method:"GET",url:"/member/address"}),s=d=>e({method:"GET",url:`/member/address/${d}`}),m=(d,r)=>e({method:"PUT",url:`/member/address/${d}`,data:r}),a=d=>e({method:"DELETE",url:`/member/address/${d}`});export{s as a,d as b,a as d,r as g,m as p};
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,31 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import { resolve } from 'path'
+
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     // 开发阶段启用源码映射：https://uniapp.dcloud.net.cn/tutorial/migration-to-vue3.html#需主动开启-sourcemap
     sourcemap: process.env.NODE_ENV === 'development',
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : "";
+          // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+          // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+          );
+        },
+      },
+    },
   },
   plugins: [uni()],
   resolve: {
